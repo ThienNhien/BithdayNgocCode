7c02357c183681991268a9e51f689b3fbbef1899
huynhthiennhien <huynhthiennhien98@gmail.com>
2022-02-20 11:14:30 +0700

    push

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hé Lô ! 🖐🖐🖐',
+    introDesc: `hôm nay có điều này muốn nói quá nè`,
+    btnIntro: 'OK',
+    title: 'Dù biết sinh nhật qua rồi nhưng mà ... 😙',
+    desc: 'Chúc Ngọc tuổi mới nhiều năng lượng mới, có nhiều niềm vui, hạnh phúc và đạt được những dự định đặt ra nhaaaaa :> ',
+    btnYes: 'Hí hí, cám ơn bé Nhiên đáng êu xinh đẹp nà 💖💖💖',
+    btnNo: '💔 Sinh nhật lâu rồi mà giờ mới có quà là saoooo ! 💔 ',
+    question:'bạn có vui khi nhận món quà này không, mình code lâu lắm mới ra đó 😊😊',
+    reasonPlaceholder: 'Rep',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'chời ơi vui quá chời quá đất luôn, cảm động sắp khóc rồi nè Ahiiiii 😊😊',
+    mess: 'Mị biết mà há há 🥰 phải cảm động thoai 😘😘',
+    messDesc: '💬💬 Đại gia nhớ sau này bao nuôi mị nha nha nha 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/thiennhien261298katmun'
+}
